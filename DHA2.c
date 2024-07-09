f553edfa0b5d5ee9f972d106dc4e442b3af2dc8d
agam2207 <agamyadav00.ay@gmail.com>
2024-07-09 23:30:24 +0530

    Update DHA2.c

--- DHA2.c
+++ DHA2.c
@@ -1,6 +1,4 @@
-//Write the entire process of compilation of a .c file to .exe file.
-Show and verify each process step by step by terminal commands.
-Assuming our .c file is named `hello.c` and contains the classic "Hello, World!" program:
+//Write the entire process of compilation of a .c file to .exe file.Show and verify each process step by step by terminal commands.Assuming our .c file is named `hello.c` and contains the classic "Hello, World!" program:
 
 #include <stdio.h>
 
